077fd595023873562cdd43a5f675ffee7a5113f6
nicspiegelhauer <Nicolaj.spiegel@gmail.com>
2020-03-31 12:01:00 +0200

    Big update!!!!

--- ClientUDP.py
+++ ClientUDP.py
@@ -1,10 +1,5 @@
 import socket
 
-print('Starting Client Server...')
-
-# Messages counter
-mc = 0
-
 # Creating a UPD socket using .SOCK_DGRAM
 soc = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 # Get the hostname, IP Address from socket and sets Portnumber
@@ -12,48 +7,51 @@ shost = socket.gethostname()
 ip = socket.gethostbyname(shost)
 lc = 'LocalHost'
 port = 5052
-
 # Setting the server address to localhost
 s_addr = (lc, port)
+# Messages counter
+mc = 0
+_3whs = False
 
-Ccom = "C: com -"
-Scom = "S: com -"
-msg = "msg"
+try:
 
-# Using .sendto to let the server know it wants to connect
-sent = soc.sendto('{}'.format(ip).encode(), s_addr)
+    # first com-0 conncetion send to server
+    con_requ = 'com-' + str(mc) + ' ' + ip
+    # Using .sendto to let the server know it wants to connect
+    sent_con_requ = soc.sendto(con_requ.encode(), s_addr)
 
-accpt, address = soc.recvfrom(5000)
+    info, server = soc.recvfrom(4096)
+    # get com-0 accept from server
+    info_string = info.decode()
+    ip_split = info_string.replace('com-0 accept ', '')
+    s_ip = ip_split
+    # Checks it messages contains 'com-0 accept' and that the IP is valid
+    if "com-0 accept" in info_string and socket.inet_aton(s_ip):
+        client_accept = 'com-' + str(mc) + ' accept'
+        sent = soc.sendto(client_accept.encode(), s_addr)
+        _3whs = True
 
-while True:
-    if accpt.startswith(b'accepted'):
+finally:
+    # If connection to server not accepted close
+    if not _3whs:
+        print('SOCKET ERROR!')
+        soc.close()
+        exit()
 
-        # print('You have been', accpt.decode(), 'by the server!')
-
-        sentaccpt = soc.sendto(Ccom.encode(), s_addr)
-
-        # Input field for username on client side
-        name = input('Enter unsername: ')
-        print('{} has joined...'.format(name))
-        # Using .sendto to send username to server
-        sent = soc.sendto(name.encode(), s_addr)
-
-        # Loop to check for messages
-        while True:
-
-            message = input('\nEnter Messages: ')
-            print(name, ':', message)
-
-            # Sends out messages to server using .sendto and using .encode to convert it to bytes.
-            sentMessages = soc.sendto(message.encode(), s_addr)
-            # Adds 1 to messages send
-            mc += 1
-
-            # Receives messages from server using .recv and making it readable using .decode
-            Smessages, server = soc.recvfrom(5000)
-            print('Server :', '{}'.format(Smessages.decode()))
-            mc += 1
-
+# loop for messages
+while _3whs:
+    message = input('Enter message: ')
+    msg = 'msg-' + str(mc) + '=' + message
+    # Sends first 'msg-' message to server
+    sent_msg = soc.sendto(msg.encode(), s_addr)
+    # Receive message from server
+    resp, server = soc.recvfrom(4096)
+    server_resp = resp.decode()
+    check_mc = int(server_resp[4])
+    mc = (int(server_resp[4]) + 1)
+    # Checks if msg counter in 'res-0' is valid
+    if mc - check_mc == 1 and 'res-' in server_resp:
+        # Gets output from server and prints it
+        print(server_resp[6:])
     else:
-        print('You were not accepted by server, goodbye!')
-        break
+        print('Msg counter ERROR')
--- ServerUDP.py
+++ ServerUDP.py
@@ -1,82 +1,75 @@
 import socket
 
-print('Setup Server...')
-
-# Server name
-name = "The Coolest Server on : "
-
-Ccom = "C: com -"
-Scom = "S: com -"
-msg = "C: msg -"
-res = "S: res -"
-
-mc = 0
-
 # Creating a UPD socket using .SOCK_DGRAM and sets Port number
 soc = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 port = 5052
 lc = 'LocalHost'
-ip = socket.gethostbyname(lc)
+ip = socket.gethostbyname(socket.gethostname())
 # Setting the server address to localhost and what port it's running on
 s_addr = (lc, port)
 # Using .bind to 'bind' the server to the socket
 soc.bind(s_addr)
-print(name, s_addr)
 
-print("Waiting for Client connections....")
 
-while True:
-    sent = soc.recv(5000)
+def _3whs_connection():
 
-    # Receives IP address from client
-    C_ip = sent.decode()
-
-    print(Ccom, mc, '<{}>'.format(C_ip))
-
-    # Split IP address
-    nsent = sent.decode().split('.', 3)
-
-    # Converts IP from str to int
-    firstIPNumber = (int(nsent[0]))
-    secoundIPNumber = (int(nsent[1]))
-    thirdIPNumber = (int(nsent[2]))
-    fourthIPNumber = (int(nsent[3]))
-
-    # Checks if received IP address is legal
-    if (0 <= firstIPNumber <= 255) and (0 <= secoundIPNumber <= 255) and (0 <= thirdIPNumber <= 255) and (0 <= fourthIPNumber <= 255):
-
-        print(Scom, mc, 'Accept', '<{}>'.format(ip))
-
-        # Sends back a accpted messages to client using .sendto
-        accpt, address = soc.recvfrom(5000)
-        accpt = soc.sendto('accepted'.encode(), address)
-
-        aceptsent = soc.recv(5000)
-        if aceptsent.startswith(b'C:'):
-            print(aceptsent.decode(), mc, 'Accept')
-
-            # Use .recv to get the clients username and prints it
-            c_name = soc.recv(5000)
-            print(Scom, mc, c_name.decode(), ':', 'has connected.')
-
-            mc += 1
-
+    # First connection form client received 'com-0'
+    info, address = soc.recvfrom(4096)
+    first_info = info.decode()
+    print(first_info)
+    # Separates ip from message
+    ip_split = first_info.replace('com-0 ', '')
+    c_ip = ip_split
+    # Checks if messages received contains 'com-0' and if IP is valid
+    if "com-0" in first_info and socket.inet_aton(c_ip):
+        server_accepts_client = 'com-0 accept' + ' ' + ip
+        # Sends back 'com-0 accept' to client
+        sent = soc.sendto(server_accepts_client.encode(), address)
+        print(server_accepts_client)
+        # Third 'com-0' messages received from client
+        info2, address = soc.recvfrom(4096)
+        if 'com-0 accept' in info2.decode():
+            print(info2.decode())
+            _1st_msg_checkup()
     else:
-        print("No good ip")
+        print('Msg counter ERROR')
 
-    # Loop to check for messages
+
+# Function check if first message is valid by 'msg-0'
+def _1st_msg_checkup():
+    message, address = soc.recvfrom(4096)
+    message_from_client = message.decode()
+    # Checks if message from client contains 'msg-0'
+    if 'msg-0' in message_from_client:
+        # Gives and sets the messages from client to next Functions(messages_sent) parameter
+        messages_sent(message_from_client, address)
+        msg_fnkt()
+    else:
+        print('Msg counter ERROR')
+
+
+# Function for sending automated reply to client
+def messages_sent(msg_from_client, c_address):
+    check_mc = int(msg_from_client[4])
+    mc = (int(msg_from_client[4]) + 1)
+    # Checks if msg counter in 'msg-0' is valid
+    if mc - check_mc == 1 and 'msg-' in msg_from_client:
+        reply = 'res-' + str(mc) + '=I am server'
+        # Automated reply 'I am server' send back to client
+        respond_to_client = soc.sendto(reply.encode(), c_address)
+    else:
+        print('Msg counter ERROR')
+
+
+# Receive and read client message and send it to Function(messages_sent)
+def msg_fnkt():
     while True:
-        print('\nWaiting to receive message from Client:')
-        # Cmessages is the clients input and where it is stored and then printed
-        Cmessages, address = soc.recvfrom(5000)
-        if mc < 1:
-            print('Counter error!')
-        else:
-            print(msg, mc, c_name.decode(), ':', Cmessages.decode())
-            mc += 1
-            print(res, mc, 'I AM A SERVER!')
+        messages, address = soc.recvfrom(4096)
+        # Receive and decodes client message
+        msg_from_client = messages.decode()
+        # Send message to get Checked
+        messages_sent(msg_from_client, address)
 
-        if Cmessages:
-            # The server sends back a automated reply using .sendto and .encode to convert it to bytes.
-            sent = soc.sendto('I AM A SERVER!!'.encode(), address)
+
+_3whs_connection()
 
